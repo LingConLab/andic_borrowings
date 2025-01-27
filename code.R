1e8327caa21a771b8f3b35a1778caa60f8a2d925
George Moroz <agricolamz@gmail.com>
2025-01-27 09:38:10 +0300

    update

---
+++ code.R
@@ -0,0 +1,32 @@
+# setwd("/home/agricolamz/work/articles/2025_russian_borrowings_in_andi_with_Vika_Chiara")
+library(tidyverse)
+
+df <- read_csv("avar_andic_rus_bor.csv")
+
+
+# ca analysis -------------------------------------------------------------
+
+df |> 
+  select(language, reference, russian_ipa, target_ipa) |> 
+  mutate(language_ref = str_c(language, ": ", reference),
+         russian_ipa = str_to_lower(russian_ipa),
+         russian_ipa = str_remove_all(russian_ipa, "'"),
+         target_ipa = str_remove_all(target_ipa, "'"),
+         russian_ipa = str_split(russian_ipa, "-"),
+         target_ipa = str_split(target_ipa, "-")) |> 
+  unnest(c(russian_ipa, target_ipa)) |> 
+  count(language_ref, russian_ipa, target_ipa) |> 
+  mutate(corresp = str_c(russian_ipa, ":", target_ipa)) |> 
+  select(-russian_ipa, -target_ipa) |> 
+  pivot_wider(names_from = corresp, values_from = n, values_fill = 0) |> 
+  column_to_rownames("language_ref") |> 
+  ca::ca() ->
+  ca
+
+ca$rowcoord |> 
+  as.data.frame() |> 
+  rownames_to_column("settlement") |> 
+  ggplot(aes(Dim1, Dim2, label = settlement))+
+  geom_point()+
+  ggrepel::geom_label_repel()+
+  theme_minimal()
