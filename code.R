11f1f3e5f652bb78955cfdbb6ffea96858a587f5
George Moroz <agricolamz@gmail.com>
2025-02-25 17:46:22 +0300

    update

--- code.R
+++ code.R
@@ -1,13 +1,14 @@
 # setwd("/home/agricolamz/work/articles/2025_russian_borrowings_in_andi_with_Vika_Chiara")
 library(tidyverse)
 
-df <- read_csv("avar_andic_rus_bor.csv")
+df <- read_csv("avar_andic_rus_ranks.csv")
 
 
 # ca analysis -------------------------------------------------------------
 
 df |> 
-  select(language, reference, russian_ipa, target_ipa) |> 
+  filter(is.na(derived)) |> 
+  select(language, reference, russian_ipa, target_ipa) |>
   mutate(language_ref = str_c(language, ": ", reference),
          russian_ipa = str_to_lower(russian_ipa),
          russian_ipa = str_remove_all(russian_ipa, "'"),
@@ -18,6 +19,7 @@ df |>
   count(language_ref, russian_ipa, target_ipa) |> 
   mutate(corresp = str_c(russian_ipa, ":", target_ipa)) |> 
   select(-russian_ipa, -target_ipa) |> 
+  distinct() |> 
   pivot_wider(names_from = corresp, values_from = n, values_fill = 0) |> 
   column_to_rownames("language_ref") |> 
   ca::ca() ->
